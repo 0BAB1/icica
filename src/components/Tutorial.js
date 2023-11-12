c5fc7248b44f30de68a8adf3f948d78fd8c30272
BRH <hb.hugobabin@gmail.com>
2023-11-12 11:35:54 +0100

    tweak tutorial section

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -2,15 +2,20 @@ import QandA from './QandA';
 
 function Tutorial() {
     return (
-        <div className='Tutorial'>
-            <QandA
-                question={"🤔 Comment ça marche ?"}
-                answer={"🚀 Vous entrez un mot de 5 caractères max.🤖 Le moteur graphique d'une complexité hors du commun vous génère une image personalisée qui vous ressemble 😊"}
-            />
-            <QandA
-                question={"💡Pourquoi utiliser icica ?"}
-                answer={"parceque. 🤓"}
-            />
+        <div className='tuto-flex'>
+            <div className='tuto-wrapper'>
+                <h1 className='tuto-title'>Comment ça marche ?</h1>
+                <div className='Tutorial'>
+                    <QandA
+                        question={"🤔 Comment ça marche ?"}
+                        answer={"🚀 Vous entrez un mot de 5 caractères max.🤖 Le moteur graphique d'une complexité hors du commun vous génère une image personalisée qui vous ressemble 😊"}
+                    />
+                    <QandA
+                        question={"💡Pourquoi utiliser icica ?"}
+                        answer={"parceque. 🤓"}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
